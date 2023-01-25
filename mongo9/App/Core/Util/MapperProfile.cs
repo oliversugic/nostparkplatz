efb89186259a1746cb6d27e980fcbce546255a81
MichaelTran6624 <michael.tran6624@gmail.com>
2023-01-25 14:38:40 +0100

    tran - modify MapperProfile.cs

--- mongo9/App/Core/Util/MapperProfile.cs
+++ mongo9/App/Core/Util/MapperProfile.cs
@@ -1,8 +1,14 @@
 ﻿using AutoMapper;
-using MongoDBDemoApp.Core.Workloads.Comments;
-using MongoDBDemoApp.Core.Workloads.Posts;
-using MongoDBDemoApp.Model.Comment;
-using MongoDBDemoApp.Model.Post;
+using MongoDBDemoApp.Core.Workloads.Competence;
+using MongoDBDemoApp.Core.Workloads.Exam;
+using MongoDBDemoApp.Core.Workloads.Student;
+using MongoDBDemoApp.Core.Workloads.Subject;
+using MongoDBDemoApp.Core.Workloads.Teacher;
+using MongoDBDemoApp.Model.Competence;
+using MongoDBDemoApp.Model.Exam;
+using MongoDBDemoApp.Model.Student;
+using MongoDBDemoApp.Model.Subject;
+using MongoDBDemoApp.Model.Teacher;
 
 namespace MongoDBDemoApp.Core.Util;
 
@@ -10,9 +16,15 @@ public sealed class MapperProfile : Profile
 {
     public MapperProfile()
     {
-        CreateMap<Post, PostDto>()
+        CreateMap<Student, StudentDTO>()
             .ForMember(p => p.Id, c => c.MapFrom(p => p.Id.ToString()));
-        CreateMap<Comment, CommentDto>()
+        CreateMap<Teacher, TeacherDTO>()
+            .ForMember(p => p.Id, c => c.MapFrom(p => p.Id.ToString()));
+        CreateMap<Exam, ExamDTO>()
+            .ForMember(p => p.Id, c => c.MapFrom(p => p.Id.ToString()));
+        CreateMap<Subject, SubjectDTO>()
+            .ForMember(p => p.Id, c => c.MapFrom(p => p.Id.ToString()));
+        CreateMap<Competence, CompetenceDTO>()
             .ForMember(p => p.Id, c => c.MapFrom(p => p.Id.ToString()));
     }
 }
