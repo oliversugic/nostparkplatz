55bfd248cd7089d73eba613c9a3e8f9ed5d1f3cc
MichaelTran6624 <michael.tran6624@gmail.com>
2023-01-25 14:38:17 +0100

    tran - add SubjectDTO

---
+++ mongo9/App/Model/Subject/SubjectDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace MongoDBDemoApp.Model.Subject;
+
+public class SubjectDTO
+{
+    public string Id { get; set; } = default!;
+    public string Name { get; set; } = default!;
+    public List<Core.Workloads.Competence.Competence> Competences { get; set; } = default!;
+}
