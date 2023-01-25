970a5e8b9498750ad6cc12012cf3b1d6523478f3
MichaelTran6624 <michael.tran6624@gmail.com>
2023-01-25 14:31:58 +0100

    tran - modify Exam entity and repository

--- mongo9/App/Core/Workloads/Exam/Exam.cs
+++ mongo9/App/Core/Workloads/Exam/Exam.cs
@@ -6,6 +6,11 @@ public class Exam: EntityBase
 {
     public Student.Student Student { get; set; } = default!;
     public Teacher.Teacher Teacher { get; set; } = default!;
-    
-    
+    public Subject.Subject Subject { get; set; } = default!;
+    public int Grade { get; set; } = default!;
+    public bool PassedExam { get; set; } = default!;
+    public DateTime Date { get; set; } = default!;
+    public int Attempt { get; set; } = default!;
+    public List<Competence.Competence> Competence { get; set; } = default!;
+
 }
--- mongo9/App/Core/Workloads/Exam/ExamRepository.cs
+++ mongo9/App/Core/Workloads/Exam/ExamRepository.cs
@@ -1,6 +1,36 @@
-﻿namespace MongoDBDemoApp.Core.Workloads.Exam;
+﻿using LeoMongo.Database;
+using LeoMongo.Transaction;
+using MongoDB.Bson;
 
-public class ExamRepository
+namespace MongoDBDemoApp.Core.Workloads.Exam;
+
+public class ExamRepository : RepositoryBase<Exam>, IExamRepository
 {
     
+    public ExamRepository(ITransactionProvider transactionProvider,
+        IDatabaseProvider databaseProvider) : base(transactionProvider, databaseProvider)
+    {
+    }
+
+    public override string CollectionName { get; } = default!;
+    public Task<Exam> AddExam(Exam comment)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Exam?> GetExamById(ObjectId id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IReadOnlyCollection<Exam>> GetAllExam()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task DeleteExam(ObjectId examId)
+    {
+        throw new NotImplementedException();
+    }
+
 }
