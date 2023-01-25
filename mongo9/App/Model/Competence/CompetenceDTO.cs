d85a27617f2cb59c9bad47f57843b00e928a9647
MichaelTran6624 <michael.tran6624@gmail.com>
2023-01-25 14:37:54 +0100

    tran - add CompetenceDTO

---
+++ mongo9/App/Model/Competence/CompetenceDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace MongoDBDemoApp.Model.Competence;
+
+public class CompetenceDTO
+{
+    public string Id { get; set; } = default!;
+    public string Compentences { get; set; } = default!;
+    public string Descripton { get; set; } = default!;
+}
